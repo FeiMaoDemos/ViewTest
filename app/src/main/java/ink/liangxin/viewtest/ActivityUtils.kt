d7d9eb422bffdf21e34fc292e8d99641f7a31e26
liangxin1988 <jay_111@163.com>
2022-07-14 14:53:43 +0800

    更新页面列表工具类

--- app/src/main/java/ink/liangxin/viewtest/ActivityUtils.kt
+++ app/src/main/java/ink/liangxin/viewtest/ActivityUtils.kt
@@ -1,7 +1,15 @@
 package ink.liangxin.viewtest
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 /**
  * 启动Activity
@@ -15,3 +23,49 @@ fun Class<out Activity>.open(activity: Activity) {
         )
     )
 }
+
+/**
+ * 使用Activity集合数据初始化RecyclerView列表
+ *
+ * @param context
+ */
+fun RecyclerView.initActivityList(context: Context) {
+    layoutManager = LinearLayoutManager(context)
+    adapter = myAdapter
+    addItemDecoration(DividerItemDecoration(context, LinearLayoutManager.VERTICAL))
+}
+
+/**
+ * Activity集合
+ */
+private val activitys = listOf<Pair<String, Class<out Activity>>>(
+    "View位置API测试" to ViewLocationActivity::class.java
+)
+
+/**
+ * 用来填充Activity集合数据的Adapter
+ */
+private val myAdapter by lazy {
+    MyAdapter()
+}
+
+private class MyAdapter : RecyclerView.Adapter<MyViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        MyViewHolder(TextView(parent.context).apply {
+            layoutParams =
+                RecyclerView.LayoutParams(RecyclerView.LayoutParams.MATCH_PARENT, 200)
+            gravity = Gravity.CENTER
+        })
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        (holder.itemView as TextView).text = activitys[position].first
+        holder.itemView.setOnClickListener {
+            activitys[position].second.open(holder.itemView.context as Activity)
+        }
+    }
+
+    override fun getItemCount() = activitys.size
+
+}
+
+private class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
--- app/src/main/java/ink/liangxin/viewtest/MainActivity.kt
+++ app/src/main/java/ink/liangxin/viewtest/MainActivity.kt
@@ -1,24 +1,10 @@
 package ink.liangxin.viewtest
 
-import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
-
-    /**
-     * Activity集合
-     */
-    private val activitys = listOf<Pair<String, Class<out Activity>>>(
-        "View位置API测试" to ViewLocationActivity::class.java
-    )
 
     /**
      * 入口列表
@@ -27,37 +13,11 @@ class MainActivity : AppCompatActivity() {
         findViewById<RecyclerView>(R.id.recycler_view)
     }
 
-    private val adapter by lazy {
-        MyAdapter()
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = adapter
-        recyclerView.addItemDecoration(DividerItemDecoration(this, LinearLayoutManager.VERTICAL))
+        recyclerView.initActivityList(this)
     }
 
-    inner class MyAdapter : RecyclerView.Adapter<MyViewHolder>() {
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-            MyViewHolder(TextView(this@MainActivity).apply {
-                layoutParams =
-                    RecyclerView.LayoutParams(RecyclerView.LayoutParams.MATCH_PARENT, 200)
-                gravity = Gravity.CENTER
-            })
-
-        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-            (holder.itemView as TextView).text = activitys[position].first
-            holder.itemView.setOnClickListener {
-                activitys[position].second.open(this@MainActivity)
-            }
-        }
-
-        override fun getItemCount() = activitys.size
-
-    }
-
-    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 }
